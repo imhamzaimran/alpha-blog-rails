7fb07ef43b6ec353fd92842ea0309c8417ed20c4
imhamzaimran <imhamzaimran@gmail.com>
2022-08-24 12:30:32 +0500

    Add user signup form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_183007) do
+ActiveRecord::Schema.define(version: 2022_08_24_065059) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_08_23_183007) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
